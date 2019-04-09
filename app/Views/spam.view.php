f611feeba7aca6463e5dbd1fcf7ac242a5e430e6
AC Ük <acwebdesign01@gmail.com>
2019-04-09 14:46:01 +0200

    Add spam example

---
+++ app/Views/spam.view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Spam</title>
+    <link rel="stylesheet" href="public/css/app.css">
+</head>
+
+<body>
+    <h1 class="welcome">Spam</h1>
+
+    <script src="public/js/app.js"></script>
+</body>
+
+</html>
--- routes.php
+++ routes.php
@@ -5,4 +5,5 @@ $router = new Router();
 $router->define([
     '' => 'app/Controllers/WelcomeController.php',
     'about' => 'app/Controllers/AboutController.php',
+    'spam' => 'app/Controllers/SpamController.php',
 ]);
