b76649a68e6cc183c72e4b5e3d7b1bf13227319c
AC Ük <acwebdesign01@gmail.com>
2019-04-10 12:02:42 +0200

    add new oform and formt csss

--- app/Controllers/ValidationController.php
+++ app/Controllers/ValidationController.php
@@ -1,7 +1,8 @@
 <?php
+var_dump($_SERVER['REQUEST_METHOD']);
+var_dump($_POST);
 if (sizeof($_POST) === 0) {
     require 'app/Views/form.view.php';
 } else {
-    trim($_POST['name']);
     require 'app/Views/validation.view.php';
 }
