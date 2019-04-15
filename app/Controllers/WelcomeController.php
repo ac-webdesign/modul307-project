d194dc00659b6ac44f30158876521c046db7098a
AC BYOD Lenovo <acwebdesign01@gmail.com>
2019-04-15 16:23:13 +0200

    -Correct link -Format

--- app/Controllers/WelcomeController.php
+++ app/Controllers/WelcomeController.php
@@ -1,2 +1,2 @@
 <?php
-require 'app/Views/welcome.view.php';
+require 'app/views/welcome.view.php';
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,8 +4,8 @@ require 'core/helpers.php';
 require 'core/database.php';
 
 // Models
-require '../app/models/repairs.php';
-require '../app/models/tools.php';
+require 'app/models/repairs.php';
+require 'app/models/tools.php';
 
 // Routers
 require 'core/Router.php';
