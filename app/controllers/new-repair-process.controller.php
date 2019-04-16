4d1185e76ef3ce489330f13ab2f98e22938e6006
AC BYOD Lenovo <acwebdesign01@gmail.com>
2019-04-16 09:31:09 +0200

    add test code & prepare for release

--- app/controllers/new-repair-process.controller.php
+++ app/controllers/new-repair-process.controller.php
@@ -1,2 +1,7 @@
 <?php
+$db = connectToDatabase();
+$tools = $this->db->prepare('SELECT * FROM `tools`');
+$tools->execute();
+var_dump($tools->fetchAll());
+
 require 'app/views/new-repair-process.view.php';
--- core/database.php
+++ core/database.php
@@ -8,7 +8,11 @@
 function connectToDatabase()
 {
     try {
-        return new PDO('mysql:host=127.0.0.1;dbname=kurseictbz_30702', 'kurseictbz_30702', 'db_307_pw_02', [
+        // return new PDO('mysql:host=dedi3038.secure.exenti.ch', 'kurseictbz_30702', 'db_307_pw_02', [
+        //     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        //     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        // ]);
+        return new PDO('mysql:localhost', 'root', '', [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
         ]);
