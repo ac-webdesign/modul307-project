7cdc65b83a6cee9a4ab6250d145983ab3f70f9f9
AC BYOD Lenovo <acwebdesign01@gmail.com>
2019-04-16 14:29:44 +0200

    get Data of Repairs

--- app/views/overview.view.php
+++ app/views/overview.view.php
@@ -11,52 +11,54 @@
     <section class="section">
         <h1 class="title">Reparaturwerkstatt</h1>
         <div class="container">
-            <h2 class="subtitle">Vorgänge</h2>
-            <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title">
-                        Auftrag ID
-                    </p>
-                </header>
-                <div class="card-content">
-                    <div class="columns">
-                        <div class="column is-one-third">
-                            <strong>Auftraggeber:</strong>
+        <h2 class="subtitle">Vorgänge</h2>
+            <?php foreach ($repairs as $repair) : ?>
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                           Auftrag <?= $repair['id'] ?>
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="columns">
+                            <div class="column is-one-third">
+                                <strong>Auftraggeber:</strong>
+                            </div>
+                            <div class="column is-one-third">
+                                <?= $repair['firstname'] ?>
+                                <?= $repair['lastname'] ?>
+                            </div>
                         </div>
-                        <div class="column is-one-third">
-                            Name
+                        <div class="columns">
+                            <div class="column is-one-third">
+                                <strong>Werkzeug:</strong>
+                            </div>
+                            <div class="column is-one-third">
+                                <?= $repair['fk_tool'] ?>
+                            </div>
+                        </div>
+                        <div class="columns">
+                            <div class="column is-one-third">
+                                <strong>Reparatur voraussichtlich abgeschlossen am:</strong>
+                            </div>
+                            <div class="column is-one-third">
+                                <?= date_format(new DateTime($repair['startdate']), 'd.m.Y'); ?>
+                            </div>
+                        </div>
+                        <div class="columns">
+                            <div class="column is-one-third">
+                                <strong>Reparatur Status:</strong>
+                            </div>
+                            <div class="column is-one-third">
+                                <i class="<?= $repair['is_done'] === '1' ? 'icon-thumbs-up' : 'icon-thumbs-down' ?>"></i>
+                            </div>
                         </div>
                     </div>
-                    <div class="columns">
-                        <div class="column is-one-third">
-                            <strong>Werkzeug:</strong>
-                        </div>
-                        <div class="column is-one-third">
-                            Werkzeug name
-                        </div>
-                    </div>
-                    <div class="columns">
-                        <div class="column is-one-third">
-                            <strong>Reparatur voraussichtlich abgeschlossen am:</strong>
-                        </div>
-                        <div class="column is-one-third">
-                            Date
-                        </div>
-                    </div>
-                    <div class="columns">
-                        <div class="column is-one-third">
-                            <strong> Reparatur Status::</strong>
-                        </div>
-                        <div class="column is-one-third">
-                            <i class="icon-thumbs-up"></i>
-                            <i class="icon-thumbs-down"></i>
-                        </div>
-                    </div>
+                    <footer class="card-footer">
+                        <a href="edit" class="card-footer-item">Edit</a>
+                    </footer>
                 </div>
-                <footer class="card-footer">
-                    <a href="edit" class="card-footer-item">Edit</a>
-                </footer>
-            </div>
+            <?php endforeach; ?>
         </div>
     </section>
 
