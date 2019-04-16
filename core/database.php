9b4b7f18c3a8a008ee0dad7aa7081613d50a5a8a
AC BYOD Lenovo <acwebdesign01@gmail.com>
2019-04-16 09:40:43 +0200

    add correct connection to local

--- core/database.php
+++ core/database.php
@@ -8,12 +8,12 @@
 function connectToDatabase()
 {
     try {
-        // return new PDO('mysql:host=dedi3038.secure.exenti.ch', 'kurseictbz_30702', 'db_307_pw_02', [
+        // return new PDO('mysql:host=dedi3038.secure.exenti.ch;dbname=kurseictbz_30702', 'kurseictbz_30702', 'db_307_pw_02', [
         //     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         //     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
         // ]);
-        return new PDO('mysql:host=localhost', 'kurseictbz_30702', 'root', '', [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        return new PDO('mysql:host=localhost;dbname=kurseictbz_30702', 'root', '', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
         ]);
     } catch (PDOException $e) {
