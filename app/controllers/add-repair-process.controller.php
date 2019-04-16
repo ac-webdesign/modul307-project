d5d60a1a735e201a11bc22f42282e571012da374
AC BYOD Lenovo <acwebdesign01@gmail.com>
2019-04-16 13:29:33 +0200

    add urgent array become day als input

--- app/controllers/add-repair-process.controller.php
+++ app/controllers/add-repair-process.controller.php
@@ -1,5 +1,28 @@
 <?php
 // TODO Validation
+$urgents = [
+    'urgent1' => array(
+        'name' => 'sehr tief',
+        'number' => 25,
+    ),
+    'urgent2' => array(
+        'name' => 'tief',
+        'number' => 20,
+    ),
+    'urgent3' => array(
+        'name' => 'normal',
+        'number' => 15,
+    ),
+    'urgent4' => array(
+        'name' => 'hoch',
+        'number' => 10,
+    ),
+    'urgent5' => array(
+        'name' => 'sehr hoch',
+        'number' => 5,
+    ),
+];
+
 $startDate = $_POST['start-date'];
 $urgent = $_POST['urgent'];
 $isDone = $_POST['is-done'];
@@ -9,5 +32,13 @@ $lastname = trim($_POST['lastname']);
 $email = trim($_POST['email']);
 $tel = trim($_POST['tel']) ?? null;
 
+foreach ($urgents as $ur => $u) {
+    if ($u['name'] === $urgent) {
+        $urgent = $u['number'];
+    }
+}
+
 $newRepairProcess = new Repairs($firstname, $lastname, $email, $tel, $urgent, $isDone, $startDate, $tool);
 $newRepairProcess->create();
+
+header('/');
