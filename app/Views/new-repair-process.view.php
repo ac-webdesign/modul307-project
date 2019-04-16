ceb8d6e35c1f111f3eda217edc4f206b4bb1e5e3
AC BYOD Lenovo <acwebdesign01@gmail.com>
2019-04-16 09:04:30 +0200

    add struktur

--- app/Views/new-repair-process.view.php
+++ app/Views/new-repair-process.view.php
@@ -8,11 +8,68 @@
 </head>
 
 <body>
+
     <section class="section">
         <div class="container">
             <h1 class="title">Auftrag erstellen</h1>
-            <form action="/add" method="POST">
-
+            <form action="app/controllers/add-repair-process.controller.php" method="POST">
+                <legend>
+                    <strong>Allgemein<strong>
+                </legend>
+                <br>
+                <div class="field">
+                    <label class="label">Start:</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Ende:</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Status:</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Dringlichkeit:</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Werkzeug:</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input">
+                    </div>
+                </div>
+                <br>
+                <legend>
+                    <strong>Auftraggeber<strong>
+                </legend>
+                <br>
+                <div class="field">
+                    <label class="label">Name:</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Email:</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Telefon:</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input">
+                    </div>
+                </div>
             </form>
         </div>
     </section>
