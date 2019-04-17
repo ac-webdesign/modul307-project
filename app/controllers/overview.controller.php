2d52f4ccf0a1df6020a2de80c9ec3d3d8e26dc68
AC BYOD Lenovo <acwebdesign01@gmail.com>
2019-04-17 14:15:00 +0200

    correct getAll of Repairs

--- app/controllers/overview.controller.php
+++ app/controllers/overview.controller.php
@@ -1,7 +1,7 @@
 <?php
 // Alle Reparaturaufträge aus DB abrufen
 $db = connectToDatabase();
-$repairs = $db->prepare('SELECT * FROM `repairs` ORDER BY `repairs`.`urgent` ASC');
+$repairs = $db->prepare('SELECT * FROM `repairs` ORDER BY `repairs`.`startdate` DESC');
 $repairs->execute();
 $repairs = $repairs->fetchAll();
 
